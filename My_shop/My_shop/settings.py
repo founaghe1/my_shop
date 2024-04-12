62c811a886b19fc8cad8396afff4324ee4290e3b
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2024-04-12 10:35:44 +0000

    ajout dependance

--- My_shop/My_shop/settings.py
+++ My_shop/My_shop/settings.py
@@ -77,9 +77,27 @@ WSGI_APPLICATION = 'My_shop.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'My_shop',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'founaghe',
+
+        'HOST': 'localhost',
+        # 'HOST': 'db',
+
+        'PORT': '5000',
+        'TEST': {
+            'NAME': 'myShop_test',
+        },
     }
 }
 
