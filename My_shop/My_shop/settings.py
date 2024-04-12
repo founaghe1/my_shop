9bfac7297ac7a20940ae53dfa797534a20eb2d71
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2024-04-12 19:29:17 +0000

    some changes

--- My_shop/My_shop/settings.py
+++ My_shop/My_shop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7$8t$)uymo8x5iqa6_bxct3kc(h&ki-+4!mgx=gi!#3%6738!f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
